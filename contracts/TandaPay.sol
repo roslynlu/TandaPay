82119ff066266ec196d9cd2de74614a9036633b2
Michael Wang <michaelwang0213@yahoo.com>
2019-05-24 16:04:59 -0700

    Fixed pseudocode so that the file compiles, added Phase enum

--- contracts/TandaPay.sol
+++ contracts/TandaPay.sol
@@ -1,74 +1,83 @@
 pragma solidity ^0.4.17;
 
-// mapping side of users: {0: not in group, 1: in group but not paid for any active periods, 2: in group and paid}
-
-minGroupSize = 12;
 
 contract Tandapay {
-
+    uint public minGroupSize = 12;
+    enum Phase {INACTIVE, PRE, ACTIVE, POST}
+    // userMapping code: {0: not in group, 1: in group but not paid for any active periods, 2: in group and paid}
+    
     struct Period {
-        uint phase;
+        Phase phase;
         uint startTime;
     }
 
-struct Group  {
-    uint groupId;
-    address secretary;
-    address[] userList; // For resetting the mapping when changing over all of the users
-    mapping(address => uint) userMapping; // For individual transactions with users i.e. paying, checking whether a user is in the Group
-    uint count;
-    uint premium;
-    uint maxClaim;
-    Period oldPeriod;
-    Period newPeriod;
-    uint etherBalance;
-}       
-
-Group[] public groups;
-address public administrator;
-uint public groupIndex; //count
+    struct Group  {
+        uint groupId;
+        address secretary;
+        address[] userList; // For resetting the mapping when changing over all of the users
+        mapping(address => uint) userMapping; // For individual transactions with users i.e. paying, checking whether a user is in the Group
+        uint paidPremiumCount;
+        uint premium;
+        uint maxClaim;
+        Period oldPeriod;
+        Period newPeriod;
+        uint currentPeriod; // Which Period is in the active phase
+        uint etherBalance;
+    }       
     
-function Tandpay() {
-administrator = msg.sender;
-}
-
-function makeGroup(address secretary, address[] policyholders, uint premium, uint maxClaim) {
-    require(policyholders.length >= minGroupSize);
-    require(maxClaim <= premium * policyholders.length);
-    Group memory newGroup = Group({
-        groupId: groupIndex,
-        secretary: secretary, 
-        policyholders: policyholders,
-        premium: premium,
-        maxClaim: maxClaim
-        period: 0,
-        periodStartTime: now,
-        etherBalance: 0
-    });
-    groupIndex += 1;
-    // add group
-}
-
-function sendPremium(uint groupId) payable {
-    require(groups[groupId …. is not null);
-    require(groups[groupId]... contains msg.sender);
-    require(groups[groupId].state == … pre preiod);
-    require(msg.value == groups[groupId].premium);
-    require(
-}
-
-function “A Secretary can initiate a period for policyholders to send in their premiums” {
-require(msg.sender == groups[groupId].secretary);
-
-}
-
-function “A Secretary can start the active period” {
-stuff
-}
-
-function “A Secretary can end the active period and start the post period” {
-    stuff
-}
+    Group[] groups;
+    address public administrator;
+    uint public groupIndex; //count
+        
+    function Tandpay() public {
+        administrator = msg.sender;
+    }
+    
+    function makeGroup(address secretary, address[] policyholders, uint premium, uint maxClaim) public {
+        require(policyholders.length >= minGroupSize);
+        require(maxClaim <= premium * policyholders.length);
+        
+        Group memory newGroup = Group({
+            groupId: groupIndex,
+            secretary: secretary, 
+            userList: policyholders,
+            paidPremiumCount: 0,
+            premium: premium,
+            maxClaim: maxClaim,
+            oldPeriod: Period(Phase.INACTIVE, now),
+            newPeriod: Period(Phase.PRE, now),
+            currentPeriod: 2, // Initialize group with newPeriod as the currentPeriod so that only newPeriod can be in the PRE phase
+            etherBalance: 0
+        });
+        groupIndex += 1;
+        
+        groups.push(newGroup);
+    }
+    
+    // A Secretary can initiate a period for policyholders to send in their premiums
+    function startPrePeriod(uint groupId) public{
+        require(msg.sender == groups[groupId].secretary);
+        
+    }
+    
+    // A policyholder can send a premium payment to a group
+    function sendPremium(uint groupId) public payable {
+        require(groups.length >= groupId); // Check if groupId is valid. Bobo implementation. Should be changed.
+        require(groups[groupId].userMapping[msg.sender] != 0);
+        require(groups[groupId].newPeriod.phase == Phase.PRE); // Assume any Period in pre-period phase is the newPeriod
+        require(msg.value == groups[groupId].premium);
+        // require(polyholder has not already paied premium)
+    }
+    
+    // A Secretary can start the active period
+    function startActivePeriod() public {
+        require(true);
+    }
+    
+    // A Secretary can end the active period and start the post period
+    // The secretary also has the option to continue to another period if there have been no claims
+    function endActivePeriod() public {
+        require(true);
+    }
     
 }
-
