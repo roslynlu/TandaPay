bd8cbc0190e2c176eae5440a0bec3e17dbc69e36
linhhuynhmac <khanhlinhtpnt@gmail.com>
2019-05-25 15:38:32 -0700

    added sendActivePeriod function

--- compile.js
+++ compile.js
@@ -2,10 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const path = path.resolve(__dirname, 'contracts', 'TandaPay.sol');
-const source = fs.readFileSync(path, 'utf8');
+const contractPath = path.resolve(__dirname, 'contracts', 'TandaPay.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
 
 module.exports = solc.compile(source, 1).contracts[':TandaPay'];
-
-
-
--- contracts/TandaPay.sol
+++ contracts/TandaPay.sol
@@ -5,13 +5,13 @@ contract Tandapay {
     uint public minGroupSize = 12;
     enum Phase {INACTIVE, PRE, ACTIVE, POST}
     // userMapping code: {0: not in group, 1: in group but not paid for any active periods, 2: in group and paid}
-    
+
     struct Period {
         Phase phase;
         uint startTime;
     }
 
-    struct Group  {
+    struct Group {
         uint groupId;
         address secretary;
         address[] userList; // For resetting the mapping when changing over all of the users
@@ -23,23 +23,23 @@ contract Tandapay {
         Period newPeriod;
         uint currentPeriod; // Which Period is in the active phase
         uint etherBalance;
-    }       
-    
+    }
+
     Group[] groups;
     address public administrator;
     uint public groupIndex; //count
-        
+
     function Tandpay() public {
         administrator = msg.sender;
     }
-    
+
     function makeGroup(address secretary, address[] policyholders, uint premium, uint maxClaim) public {
         require(policyholders.length >= minGroupSize);
         require(maxClaim <= premium * policyholders.length);
-        
+
         Group memory newGroup = Group({
             groupId: groupIndex,
-            secretary: secretary, 
+            secretary: secretary,
             userList: policyholders,
             paidPremiumCount: 0,
             premium: premium,
@@ -50,16 +50,16 @@ contract Tandapay {
             etherBalance: 0
         });
         groupIndex += 1;
-        
+
         groups.push(newGroup);
     }
-    
+
     // A Secretary can initiate a period for policyholders to send in their premiums
     function startPrePeriod(uint groupId) public{
         require(msg.sender == groups[groupId].secretary);
-        
+
     }
-    
+
     // A policyholder can send a premium payment to a group
     function sendPremium(uint groupId) public payable {
         require(groups.length >= groupId); // Check if groupId is valid. Bobo implementation. Should be changed.
@@ -68,16 +68,19 @@ contract Tandapay {
         require(msg.value == groups[groupId].premium);
         // require(polyholder has not already paied premium)
     }
-    
+
     // A Secretary can start the active period
-    function startActivePeriod() public {
-        require(true);
+    function startActivePeriod(uint groupId) public {
+      require(msg.sender == groups[groupId].secretary); //Only secretary can start active period
+      require(groups[groupId].paidPremiumCount == groups[groupId].userList.length); //All premiums have been paid
+      require(groups[groupId].newPeriod.phase != Phase.ACTIVE); //Group is not active
+      groups[groupId].newPeriod.phase = Phase.ACTIVE;
     }
-    
+
     // A Secretary can end the active period and start the post period
     // The secretary also has the option to continue to another period if there have been no claims
     function endActivePeriod() public {
         require(true);
     }
-    
+
 }
