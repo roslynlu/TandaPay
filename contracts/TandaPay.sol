3de64999cda74e09950f6316b20cf0403dd647c3
linhhuynhmac <khanhlinhtpnt@gmail.com>
2019-05-26 14:48:02 -0700

    added periodCount in Group, changed userMapping, added new tests

--- contracts/TandaPay.sol
+++ contracts/TandaPay.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.17;
 
 
 contract Tandapay {
-    uint public minGroupSize = 12;
+    uint public minGroupSize = 10; //Set 10 for testing purpose
     enum Phase {INACTIVE, PRE, ACTIVE, POST}
     // userMapping code: {0: not in group, 1: in group but not paid for any active periods, 2: in group and paid}
 
@@ -14,8 +14,9 @@ contract Tandapay {
     struct Group {
         uint groupId;
         address secretary;
-        address[] userList; // For resetting the mapping when changing over all of the users
-        mapping(address => uint) userMapping; // For individual transactions with users i.e. paying, checking whether a user is in the Group
+        /* Maps each policyholder to a number. Increment this number to match periodCount
+        everytime they pay the premium */
+        mapping(address => uint) userMapping;
         uint paidPremiumCount;
         uint premium;
         uint maxClaim;
@@ -23,27 +24,23 @@ contract Tandapay {
         Period newPeriod;
         uint currentPeriod; // Which Period is in the active phase
         uint etherBalance;
+        /* Keep track of period */
+        uint periodCount;
+        /* Keep track of number of policyholders in group */
+        uint policyholderCount;
     }
-
-    mapping(uint => Group) groups;
+    Group[] groups;
     address public administrator;
     uint public groupIndex; //count
-    
+
     modifier secretaryOnly(uint groupId) {
         require(groups[groupId].secretary == msg.sender);
         _;
     }
-    
-    modifier validId(uint groupId) {
-        // groupIndex starts at 1, so all created groups have groupId greater than 0
-        // I think this is how a mapping of structs works
-        require(groups[groupId].groupId != 0); 
-        _;
-    }
 
-    function Tandpay() public {
+    function Tandapay() public {
         administrator = msg.sender;
-        groupIndex = 1;
+        groupIndex = 0;
     }
 
     function makeGroup(address secretary, address[] policyholders, uint premium, uint maxClaim) public {
@@ -54,75 +51,101 @@ contract Tandapay {
         // Do we want to add a check to make sure all elements of policyholders are unique?
         // If there are duplicates, it messes up paidPremiumCount logic.
         //////////////////////////////////////////////////////////////////////////////////
-        
+
         Group memory newGroup = Group({
             groupId: groupIndex,
             secretary: secretary,
-            userList: policyholders,
             paidPremiumCount: 0,
             premium: premium,
             maxClaim: maxClaim,
             oldPeriod: Period(Phase.INACTIVE, now),
             newPeriod: Period(Phase.INACTIVE, now),
             currentPeriod: 2, // Initialize group with newPeriod as the currentPeriod so that only newPeriod can be in the PRE phase
-            etherBalance: 0
+            etherBalance: 0,
+            periodCount: 1,
+            policyholderCount: policyholders.length
         });
-        
-        groups[groupIndex] = newGroup;
-        
-        // Use userList to initialize userMapping
+
+        groups.push(newGroup);
+
         Group storage currentGroup = groups[groupIndex];
-        for (uint i = 0; i < currentGroup.userList.length; i++) {
-            currentGroup.userMapping[currentGroup.userList[i]] = 1;
+        for (uint i = 0; i < policyholders.length; i++) {
+          /* initial periodCount is 1
+          users map to 1 to indicate they have to pay premiums for period 1 */
+          currentGroup.userMapping[policyholders[i]] = 1;
         }
         require(currentGroup.userMapping[secretary] == 1); // Checks if secreatry was one of the passed in policyholders
-        
+
         groupIndex += 1;
     }
 
     // A Secretary can initiate a period for policyholders to send in their premiums
-    function startPrePeriod(uint groupId) public secretaryOnly(groupId) validId(groupId) {
+    function startPrePeriod(uint groupId) public secretaryOnly(groupId) {
         Group storage currentGroup = groups[groupId];
-        
+
         currentGroup.oldPeriod = currentGroup.newPeriod;
         currentGroup.newPeriod = Period(Phase.PRE, now);
     }
 
     // A policyholder can send a premium payment to a group
-    function sendPremium(uint groupId) public payable validId(groupId) {
-        Group storage currentGroup = groups[groupId];
-        
-        require(currentGroup.userMapping[msg.sender] == 1); // User is part of group and has not paid the premium
-        require(currentGroup.newPeriod.phase == Phase.PRE); // Assume any Period in pre-period phase is the newPeriod
-        require(msg.value == currentGroup.premium);
-        
-        currentGroup.userMapping[msg.sender] = 2;
-        currentGroup.etherBalance += msg.value;
-        currentGroup.paidPremiumCount += 1;
+    function sendPremium(uint groupId) public payable {
+      Group storage currentGroup = groups[groupId]; //If groupId is not valid, errors here
+      require(msg.value == currentGroup.premium);
+      require(currentGroup.newPeriod.phase == Phase.PRE); // Assume any Period in pre-period phase is the newPeriod
+      require(currentGroup.userMapping[msg.sender] == currentGroup.periodCount); // User is part of group and has not paid the premium
+
+      currentGroup.userMapping[msg.sender]++;
+      currentGroup.etherBalance += msg.value;
+      currentGroup.paidPremiumCount += 1;
     }
 
     // A Secretary can start the active period
-    function startActivePeriod(uint groupId) public secretaryOnly(groupId) validId(groupId){
-        Group storage currentGroup = groups[groupId];
-                
-        require(currentGroup.paidPremiumCount == currentGroup.userList.length); //All premiums have been paid
-        require(currentGroup.newPeriod.phase == Phase.PRE); //Group is not active
-        
-        currentGroup.newPeriod.phase = Phase.ACTIVE;
-        currentGroup.currentPeriod = 2;
-        // Reset userMapping
-        for (uint i = 0; i < currentGroup.userList.length; i++) {
-            currentGroup.userMapping[currentGroup.userList[i]] = 1;
-        }
-        currentGroup.paidPremiumCount = 0;
+    function startActivePeriod(uint groupId) public secretaryOnly(groupId){
+      Group storage currentGroup = groups[groupId];//If groupId is not valid, errors here
+
+      require(currentGroup.paidPremiumCount == currentGroup.policyholderCount); //All premiums have been paid
+      require(currentGroup.newPeriod.phase == Phase.PRE); //Group is not active
+
+      currentGroup.newPeriod.phase = Phase.ACTIVE;
+      currentGroup.currentPeriod = 2;
+
+      currentGroup.paidPremiumCount = 0;
+      currentGroup.periodCount++;
     }
 
     // A Secretary can end the active period and start the post period
     // The secretary also has the option to continue to another period if there have been no claims
-    function endActivePeriod(uint groupId) public secretaryOnly(groupId) validId(groupId){
+    /*function endActivePeriod(uint groupId) public secretaryOnly(groupId) {
         Group storage currentGroup = groups[groupId];
-        
+
         require(true);
+    }*/
+
+    function getGroupSecretary(uint groupId) public view returns(address) {
+        return groups[groupId].secretary;
     }
 
+    function getGroupPeriodCount(uint groupId) public view returns(uint) {
+        return groups[groupId].periodCount;
+    }
+
+    function getGroupPaidCount(uint groupId) public view returns(uint) {
+        return groups[groupId].paidPremiumCount;
+    }
+
+    function isActive(uint groupId) public view returns(bool) {
+        return (groups[groupId].newPeriod.phase == Phase.ACTIVE);
+    }
+
+    function prePeriodStart(uint groupId) public view returns(bool) {
+      return (groups[groupId].newPeriod.phase == Phase.PRE);
+    }
+
+    function isMember(uint groupId, address add) public view returns(bool){
+      return (groups[groupId].userMapping[add] != 0);
+    }
+
+    function getGroupPremium(uint groupId) public view returns(uint) {
+      return groups[groupId].premium;
+    }
 }
--- test/TandaPay.test.js
+++ test/TandaPay.test.js
@@ -1,23 +1,91 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
 let tandapay;
 let accounts;
+let admin;
+let secretary;
+let minGroupSize = 10;
 
 beforeEach( async () => {
   accounts = await web3.eth.getAccounts();
-
+  admin = accounts[0];
+  secretary = accounts[0];
+  // Deploy a contract
   tandapay = await new web3.eth.Contract(JSON.parse(interface))
     .deploy( {data: bytecode })
-    .send( {from: accounts[0], gas: '1000000 '});
+    .send( {from: admin, gas: '1000000 '});
+  // Create a new group
+  await tandapay.methods.makeGroup(secretary, accounts, 1, 1 * 10)
+    .send({from: admin, gas: '1000000'});
 });
 
 describe('TandaPay Contract', () => {
   it('deploys a contract', () => {
-    assert.ok(lottery.options.address);
+    assert.ok(tandapay.options.address);
   });
+
+  it('check admin', async () => {
+    contractAdmin = await tandapay.methods.administrator().call();
+    assert.equal(admin, contractAdmin);
+  })
+
+  it('check group secretary', async () => {
+    groupSecretary = await tandapay.methods.getGroupSecretary(0).call();
+    assert.equal(secretary, groupSecretary);
+  });
+
+  it('start pre-period', async () => {
+    await tandapay.methods.startPrePeriod(0).send({
+      from:secretary, gas: '1000000'
+    });
+    isPre = await tandapay.methods.prePeriodStart(0).call();
+    assert.equal(true, isPre);
+  });
+
+  it('is a member', async() => {
+    isMember = await tandapay.methods.isMember(0, accounts[1]).call();
+    assert.equal(true, isMember);
+  });
+
+/***********************
+** Not tested yet
+**************************/
+/*
+  it('pay a premium', async() => {
+    let premium = await tandapay.methods.getGroupPremium(0).call();
+    await tandapay.methods.sendPremium(0).send({
+      value: premium,
+      from: accounts[1]
+    });
+    paidCount = await tandapay.methods.getGroupPaidCount(0).call();
+    assert.equal(paidCount, 1);
+  });
+
+  it('all premium paid', async () => {
+    for (let acccount in accounts) {
+      await tandapay.methods.sendPremium(premium).send({
+        from: account, gas: '1000000'
+      });
+    }
+    paidCount = await tandapay.methods.getGroupPaidCount(0).call();
+    assert.equal(minGroupSize, paidCount);
+  })
+
+  it('start active period', async () => {
+    await tandapay.methods.startActivePeriod(0).send({
+      from: secretary, gas: '1000000'
+    });
+    isActive = await tandapay.methods.isActive(0).call();
+    assert.equal(true, isActive);
+  });
+*/
+
+
+
 });
