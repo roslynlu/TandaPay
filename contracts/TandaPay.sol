4a64f66a3dbd1d8c5a705f588843469df1976315
Michael Wang <michaelwang0213@yahoo.com>
2019-05-25 23:48:16 -0700

    Added secretaryOnly modifier, validId modifier, added to makeGroup(), added to startActivePeriod

--- contracts/TandaPay.sol
+++ contracts/TandaPay.sol
@@ -28,15 +28,31 @@ contract Tandapay {
     Group[] groups;
     address public administrator;
     uint public groupIndex; //count
+    
+    modifier secretaryOnly(uint groupId) {
+        require(groups[groupId].secretary == msg.sender);
+        _;
+    }
+    modifier validId(uint groupId) {
+        /////////////////////////////////////////
+        // Bobo implementation, should be changed
+        /////////////////////////////////////////
+        require(groups.length >= groupId); 
+        _;
+    }
 
     function Tandpay() public {
         administrator = msg.sender;
+        groupIndex = 0;
     }
 
     function makeGroup(address secretary, address[] policyholders, uint premium, uint maxClaim) public {
         require(policyholders.length >= minGroupSize);
         require(maxClaim <= premium * policyholders.length);
-
+        ///////////////////////////////////////////////////////////////////////////////////
+        // Do we want to add a check to make sure all elements of policyholders are unique?
+        // If there are duplicates, it messes up paidPremiumCount logic.
+        //////////////////////////////////////////////////////////////////////////////////
         Group memory newGroup = Group({
             groupId: groupIndex,
             secretary: secretary,
@@ -45,41 +61,67 @@ contract Tandapay {
             premium: premium,
             maxClaim: maxClaim,
             oldPeriod: Period(Phase.INACTIVE, now),
-            newPeriod: Period(Phase.PRE, now),
+            newPeriod: Period(Phase.INACTIVE, now),
             currentPeriod: 2, // Initialize group with newPeriod as the currentPeriod so that only newPeriod can be in the PRE phase
             etherBalance: 0
         });
-        groupIndex += 1;
-
+        
         groups.push(newGroup);
+        
+        // Use userList to initialize userMapping
+        Group storage currentGroup = groups[groupIndex];
+        for (uint i = 0; i < currentGroup.userList.length; i++) {
+            currentGroup.userMapping[currentGroup.userList[i]] = 1;
+        }
+        
+        require(currentGroup.userMapping[secretary] == 1); // Checks if secreatry was one of the passed in policyholders
+        
+        groupIndex += 1;
     }
+    
 
     // A Secretary can initiate a period for policyholders to send in their premiums
-    function startPrePeriod(uint groupId) public{
-        require(msg.sender == groups[groupId].secretary);
-
+    function startPrePeriod(uint groupId) public secretaryOnly(groupId) validId(groupId) {
+        Group storage currentGroup = groups[groupId];
+        
+        currentGroup.oldPeriod = currentGroup.newPeriod;
+        currentGroup.newPeriod = Period(Phase.PRE, now);
     }
 
     // A policyholder can send a premium payment to a group
-    function sendPremium(uint groupId) public payable {
-        require(groups.length >= groupId); // Check if groupId is valid. Bobo implementation. Should be changed.
-        require(groups[groupId].userMapping[msg.sender] != 0);
-        require(groups[groupId].newPeriod.phase == Phase.PRE); // Assume any Period in pre-period phase is the newPeriod
-        require(msg.value == groups[groupId].premium);
-        // require(polyholder has not already paied premium)
+    function sendPremium(uint groupId) public payable validId(groupId) {
+        Group storage currentGroup = groups[groupId];
+        
+        require(currentGroup.userMapping[msg.sender] == 1); // User is part of group and has not paid the premium
+        require(currentGroup.newPeriod.phase == Phase.PRE); // Assume any Period in pre-period phase is the newPeriod
+        require(msg.value == currentGroup.premium);
+        
+        currentGroup.userMapping[msg.sender] = 2;
+        currentGroup.etherBalance += msg.value;
+        currentGroup.paidPremiumCount += 1;
     }
 
     // A Secretary can start the active period
-    function startActivePeriod(uint groupId) public {
-      require(msg.sender == groups[groupId].secretary); //Only secretary can start active period
-      require(groups[groupId].paidPremiumCount == groups[groupId].userList.length); //All premiums have been paid
-      require(groups[groupId].newPeriod.phase != Phase.ACTIVE); //Group is not active
-      groups[groupId].newPeriod.phase = Phase.ACTIVE;
+    function startActivePeriod(uint groupId) public secretaryOnly(groupId) validId(groupId){
+        Group storage currentGroup = groups[groupId];
+                
+        require(currentGroup.paidPremiumCount == currentGroup.userList.length); //All premiums have been paid
+        require(currentGroup.newPeriod.phase == Phase.PRE); //Group is not active
+        
+        currentGroup.newPeriod.phase = Phase.ACTIVE;
+        currentGroup.currentPeriod = 2;
+        // Reset userMapping
+        for (uint i = 0; i < currentGroup.userList.length; i++) {
+            currentGroup.userMapping[currentGroup.userList[i]] = 1;
+        }
+        currentGroup.paidPremiumCount = 0;
     }
 
     // A Secretary can end the active period and start the post period
     // The secretary also has the option to continue to another period if there have been no claims
-    function endActivePeriod() public {
+    function endActivePeriod(uint groupId) public secretaryOnly(groupId) validId(groupId){
+        Group storage currentGroup = groups[groupId];
+        
         require(true);
     }
 
